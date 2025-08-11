8f4f0ca372c8366698406b0188b4d2529d65169b
Simar111 <simarnarula1477@gmail.com>
2025-08-11 11:02:57 -0700

    done

--- client/src/pages/Crockery.jsx
+++ client/src/pages/Crockery.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState ,useRef} from 'react';
 import axios from 'axios';
 import CrockeryGrid from '../crockerysection/CrockeryGrid';
 import CrockerySlider from '../crockerysection/CrockerySlider';
